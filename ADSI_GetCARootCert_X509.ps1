6692c9b90aab3aa98c8ba8891946c1d28f9f2bab
Ville Koch <ville.koch@2ns.fi>
2024-09-30 14:09:24 +0300

    added new script to export Root CA cert from LDAP into cer file

---
+++ ADSI_GetCARootCert_X509.ps1
@@ -0,0 +1,104 @@
+<#
+.SYNOPSIS
+    Script to export a target DCs CA root certificate from LDAP into a cer file
+
+.DESCRIPTION
+    
+    This script gets the LDAP property "caCertificate" from a target domain controller and exports it as a x509 certificate
+
+    File-Name:  ADSI_GetCARootCert_X509.ps1
+    Author:     Ville Koch (@vegvisir87, https://github.com/ville87)
+    Version:    v1.0 (30.09.2024)
+
+
+.LINK
+    https://github.com/ville87/ADSIx509
+
+.EXAMPLE
+    Get the Root CA certificate via LDAP and export it as a cer file:
+    .\ADSI_GetCARootCert_X509.ps1 -domain lab.local -dc dc1.lab.local -CertExportPath C:\users\jdoe\Desktop
+#>
+
+#################################### PARAMETERS ###########################################
+[CmdletBinding()]
+Param (
+    # domain: Domain to connect to. Should be in format domain.tld (currently no built-in validation)
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string]$domain, 
+
+    # dc: Domain controller to connect to. Should be in format hostname.domain.tld (currently no built-in validation)
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string]$dc,
+
+    # CertExportPath: Path to where the CA root cert should be stored
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [System.IO.FileInfo]$CertExportPath
+)
+
+Begin {
+    
+#################################### VARIABLES ###########################################
+
+    [string]$baseDN                 = "DC=$(($domain -split "\.")[0]),DC=$(($domain -split "\.")[1])"
+
+#################################### FUNCTIONS ###########################################
+    function printInfo { 
+        Param (
+        [Parameter(Mandatory = $true)][string]$info, # String to log
+        [Parameter(Mandatory = $true)][ValidateSet("INFO","WARNING","ERROR")][string]$level
+        )
+        if($level -eq "ERROR"){
+            Write-Host -ForegroundColor Red -BackgroundColor Black "$('[{0:HH:mm}]' -f (Get-Date)) - $level - $info"
+        }elseif($level -eq "WARNING"){
+            Write-Host -ForegroundColor Yellow -BackgroundColor Black "$('[{0:HH:mm}]' -f (Get-Date)) - $level - $info"
+        }else{
+            Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) - $level - $info"
+        }
+            
+        if($loggingenabled){
+            "$('[{0:HH:mm}]' -f (Get-Date)) - $level - $info" | Out-File -FilePath $logfilepath -Append
+        }
+    }
+
+} # Begin
+
+#################################### MAIN ################################################
+Process {
+
+    try {
+            
+        # Load the required namespace and get the AD property
+        Add-Type -AssemblyName System.DirectoryServices
+        $ldapPath = "LDAP://$dc/CN=Public Key Services,CN=Services,CN=Configuration,$baseDN"
+        $directoryEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath)
+        $searcher = New-Object System.DirectoryServices.DirectorySearcher($directoryEntry)
+        $searcher.Filter = "(objectClass=pKIEnrollmentService)"
+        $searcher.PropertiesToLoad.Add("cACertificate") | Out-Null
+        $searchResults = $searcher.FindAll()
+
+        # convert the resulting byte array to a cert
+        foreach ($result in $searchResults) {
+            $certificates = $result.Properties["cACertificate"]
+            foreach ($cert in $certificates) {
+                [byte[]] $DERCert = $cert
+                $X509CertificateCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+                $X509CertificateCollection.Import($DERCert, $null, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::EphemeralKeySet)
+                Write-Output "##############   Found cert:   ################`r`n $X509CertificateCollection"
+            }
+        }
+
+        foreach ($certCollection in $X509CertificateCollection) {
+            $cerFilePath = Join-Path $CertExportPath "$($certCollection.Thumbprint).cer"
+            $cerBytes = $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert)
+            [System.IO.File]::WriteAllBytes($cerFilePath, $cerBytes)
+            Write-Output "CER file created here: $cerFilePath"
+        }
+
+    } catch {
+        printInfo -info "There was an error when running the script. Error:`r`n$_" -level "ERROR"
+    }
+} # Process
+
+End {
+
+} # End
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # ADSIx509
 Author:     Ville Koch (@vegvisir87, https://github.com/ville87)   
-Version:    v1.0 (30.11.2023)   
+Version:    v1.1 (06.12.2023)   
 
 > [!WARNING]  
-> This script was made for testing purposes in an Active Directory domain.    
+> These scripts were made for testing purposes in an Active Directory domain.    
 > It is not advised to use this in a production environment!   
 
-This script uses x509 certificate based authentication against LDAP to create a user or take an existing user and if specified add it as a member to a specified group. 
+## ADSI_AddUser_X509.ps1
+The script `ADSI_AddUser_X509.ps1` uses x509 certificate based authentication against LDAP to create a user or take an existing user and if specified add it as a member to a specified group. 
 - The user will be enabled after creation (userAccountControl=512). 
 - The users distinguishedName will be built from the given domain name, like: `CN=Username,CN=Users,DC=lab,DC=local.`
 - When looking for users or group, the Base DN is currently always the container *CN=Users,DC=lab,DC=local*.
@@ -18,9 +19,8 @@ TODO:
 - Figure out how System.DirectoryServices.Protocols.SearchRequest has to be set to look in any container / subcontainers / OUs, not just CN=users...
 - Add more error handling
 - Add verbose logging possibility
-- Add possibility to skip user creation and only add user to group
 
-## Examples 
+### Examples 
 Create a new user and add it to the Domain Admins group:   
 ```powershell
 .\ADSI_AddUser_X509.ps1 -CertPath C:\TEMP\domadmin.pfx -domain lab.local -DCIP 10.0.0.4 -CreateUser Y -samAccountName baduser1 -givenName Hans -sn Landa -AddToGroup Y -groupName "Domain Admins"
@@ -34,4 +34,13 @@ Take an existing user and add it to the Domain Admins group:
 Only create a new user:   
 ```powershell
 .\ADSI_AddUser_X509.ps1 -CertPath C:\TEMP\domadmin.pfx -domain lab.local -DCIP 10.0.0.4 -CreateUser Y -samAccountName anotheruser1 -givenName Mister -sn Blonde -AddToGroup N
+```
+
+## ADSI_GetCARootCert_X509.ps1
+This script gets the LDAP property "caCertificate" from a target domain controller and exports it as a x509 certificate
+
+### Examples 
+Get the Root CA certificate via LDAP and export it as a cer file:   
+```powershell
+.\ADSI_GetCARootCert_X509.ps1 -domain lab.local -dc dc1.lab.local -CertExportPath C:\users\jdoe\Desktop
 ```
